ee2103260970611de8734ba65e1f84a296f94b30
maxx52 <gospelwork@yandex.ru>
2026-06-14 08:58:20 +0700

    Add header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="app-header">
+    <v-container>
+      <nav class="app-header__nav">
+        <a href="#top" class="app-header__link" aria-label="На главную"></a>
+      </nav>
+    </v-container>
+  </header>
+</template>
