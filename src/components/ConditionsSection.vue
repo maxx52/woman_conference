c0a60e6b7ee2d60d5bed906b3c4b54b3c2779ada
maxx52 <gospelwork@yandex.ru>
2026-06-14 08:59:06 +0700

    Add conditions section component

---
+++ src/components/ConditionsSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="conditions-section">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" sm="8" md="6" lg="5">
+          <div class="conditions mb-8">
+            <h3 class="h3 mb-3 text-center font-weight-bold">Условия участия</h3>
+            <p class="h6 mb-4">Для участия требуется регистрация. <br> Онлайн регистрация до 23 апреля 2026 года.</p>
+            <p class="h6 mb-4">Добровольное пожертвование на организацию конференции 500₽.</p>
+            <p class="h6 mb-2 font-weight-bold">Конференция пройдет по адресу</p>
+            <p class="mb-0">
+              <a class="h6 text-underline black-text" href="https://2gis.ru/barnaul/geo/70000001111202150" target="_blank" rel="noopener">
+                г. Барнаул, п-кт Космонавтов 12/7, 2 этаж
+              </a>
+            </p>
+            <p class="h6 mb-4">
+              Проезд до остановки «Алтайский шинный комбинат»<br>
+              От ж/д вокзала и автовокзала: <br>
+              Трамвай №1, 5; Автобус № 1, 32
+            </p>
+            <p class="h6 mb-2 font-weight-bold">Проживание</p>
+            <div class="h6 mb-4">
+              Участникам конференции необходимо самостоятельно позаботиться о проживании.
+              Если вам нужна помощь в поиске или рекомендации по проверенным вариантам, вы можете связаться с
+              региональным секретарем Мариной Сергеевной по тел.
+              <a href="tel:+79617253115" class="nowrap"><strong>+7 (961) 725-31-15</strong></a>
+            </div>
+            <p class="h6 mb-2 font-weight-bold">Питание</p>
+            <div class="h6 mb-4">Для участников будет организован обед (за счет регистрационного взноса).</div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
