fd63f2310d060af73c904898dc7f98749fbb4d23
maxx52 <gospelwork@yandex.ru>
2026-06-14 08:58:01 +0700

    Add main conference page

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <AppHeader />
+
+    <v-main>
+      <HeroSection />
+      <SpeakersSection />
+      <ConditionsSection />
+      <RegistrationForm />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import HeroSection from './components/HeroSection.vue'
+import SpeakersSection from './components/SpeakersSection.vue'
+import ConditionsSection from './components/ConditionsSection.vue'
+import RegistrationForm from './components/RegistrationForm.vue'
+import AppFooter from './components/AppFooter.vue'
+</script>
