cc207c256cdd1f66a4d3fb22af720b12eedf1675
maxx52 <gospelwork@yandex.ru>
2026-06-14 08:34:54 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <AppBar />
+
+    <v-main>
+      <HeroSection />
+      <SpeakersSection />
+      <ConditionsSection />
+      <RegistrationForm />
+    </v-main>
+
+    <Footer />
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script setup>
+  import HeroSection from './components/HeroSection.vue'
+  import SpeakersSection from './components/SpeakersSection.vue'
+  import ConditionsSection from './components/ConditionsSection.vue'
+  import RegistrationForm from './components/RegistrationForm.vue'
+</script>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { createVuetify } from 'vuetify'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import './styles/main.scss'
+
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'conferenceTheme',
+        themes: {
+            conferenceTheme: {
+                dark: false,
+                colors: {
+                    primary: '#080938',
+                    secondary: '#042e5e',
+                },
+            },
+        },
+    },
+})
+
+createApp(App).use(store).use(router).use(vuetify).mount('#app')
