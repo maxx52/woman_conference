ea91eb3bc2d7331aac5abe9a0eab2c0d9f0254c0
maxx52 <gospelwork@yandex.ru>
2026-06-14 08:59:45 +0700

    Add footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer footer-bg">
+    <div class="footer-top">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6">
+            <div class="footer-widget">
+              <h3 class="footer-title">Подпишись</h3>
+              <ul class="footer-links">
+                <li><a target="_blank" rel="noopener" href="https://vk.ru/church_the_new_testament">Сообщество ВКонтакте Церковь Новый Завет | Барнаул</a></li>
+                <li><a target="_blank" rel="noopener" href="https://vk.ru/schastlivayaplus">Сообщество ВКонтакте Женское Комьюнити | СчастливаЯ+</a></li>
+                <li><a target="_blank" rel="noopener" href="https://t.me/schastlivayaplus">Telegram Женское Комьюнити | СчастливаЯ+</a></li>
+              </ul>
+            </div>
+          </v-col>
+
+          <v-col cols="12" md="6">
+            <div class="footer-widget">
+              <h3 class="footer-title">Организатор</h3>
+              <p class="footer-organizer">Церковь «Новый Завет»<br>г. Барнаул, пр-т Космонавтов, д. 12/7</p>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+
+    <div class="copy-right-text">
+      <v-container>
+        <div class="footer-border"></div>
+        <p class="copy-right text-center shadow">
+          2026 © Религиозная группа Церковь христиан веры евангельской пятидесятников «Новый Завет» г. Барнаул уч.н. 2218010079<br>
+          <a target="_blank" href="/" class="shadow">Реквизиты организации</a> | <a target="_blank" href="/" class="shadow">Политика конфиденциальности</a>
+        </p>
+      </v-container>
+    </div>
+  </footer>
+</template>
